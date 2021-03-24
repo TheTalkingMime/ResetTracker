afd1add39b02e914727501871eb22c5e180d6e79
jan-leila <jan-leila@protonmai.com>
2021-03-24 09:12:38 -0500

    fixed chown of main file name

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ mkdir dist
 sudo chown 1000 dist
 
 sudo docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pyinstaller --onefile src/main.py"
-sudo chown 1000 dist/resetTracker.exe
+sudo chown 1000 dist/main.exe
 
 pyinstaller --onefile src/main.py
