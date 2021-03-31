e4e07824d0ee7a62ff7730a0aeaef0281d3f3ba1
jan-leila <jan-leila@protonmai.com>
2021-03-31 14:11:35 -0500

    changed build file output paths

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ sudo chown 1000 dist
 
 sudo docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pyinstaller --onefile src/main.py"
 sudo chown 1000 dist/main.exe
+cp dist/main.exe dist/reset_tracker_windows.exe
+rm dist/main.exe
 
 pyinstaller --onefile src/main.py
+cp dist/main dist/reset_tracker_linux
+rm dist/main
